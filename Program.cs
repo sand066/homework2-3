b813cdaa26ab1a7f2cbfe66672c0f641c62e6d78
sand066 <sandnattaphon@gmail.com>
2023-05-20 17:12:19 +0700

    homework3

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string numOp;
+
+    do
+    {
+      numOp = Console.ReadLine();
+      if (numOp == "+" || numOp == "-")
+      {
+
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        float[,] num = new float[n, m];
+        float[,] num2 = new float[n, m];
+
+        for ( int i = 0; i < n; i++) 
+        {
+          for (int j = 0; j < m; j++)
+          {
+            num[i, j] = float.Parse(Console.ReadLine());
+          }
+        }
+
+        for (int i = 0; i < n; i++) 
+        {
+          for (int j = 0; j < m; j++)
+          {
+            num2[i, j] = float.Parse(Console.ReadLine());
+          }
+        }
+
+        for (int i = 0; i < n; i++) 
+        {
+          for (int j = 0; j < m; j++)
+          {
+             switch (numOp)
+            {
+              case "+":
+                num[i, j] = num[i, j] + num2[i, j];
+                break;
+              case "-":
+                num[i, j] = num[i, j] - num2[i, j];
+                break;
+            }
+          }
+        }
+         for (int i = 0; i < n; i++) 
+        {
+          for (int j = 0; j < m; j++)
+          {
+            Console.Write("{0} ", num[i, j]);
+          }
+          Console.WriteLine("");
+        }
+      }
+
+    } while (numOp == "+" || numOp == "-");
+
+  }
+
+}
